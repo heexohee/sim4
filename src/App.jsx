5fd9e41c1da06bf4a3f35f4aab98f14623b30f6e
heexohee <rsoy2918@naver.com>
2023-07-01 13:44:49 +0900

    useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
+import "./App.css";
+import React, { useRef, useState } from "react";
 
-import './App.css';
-import React, { useRef } from 'react';
-
+const style = {
+  border: "1px solid black",
+  margin: "10px",
+  padding: "10px",
+}
 
 function App() {
-  const ref = useRef('초기값');
-  console.log('ref', ref);
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
 
-  ref.current = '변경값';
+const plusStateCountButtonHandler = () => {
+setCount(count + 1 );
+}
+const plusRefCountButtonHandler = () => {
+  countRef.current++;
+  console.log(countRef.current);
+}
 
   return (
-    <div className="App">
-      ㅇㅅㅇ
-    </div>
+    <>
+      <div
+        style={style}
+      >
+        state 영역입니다.{count}
+        <br />
+        <button onClick={plusStateCountButtonHandler}> state 증가</button>
+      </div>
+      {/* countRef는 객체, 객체가 만들어진 거고 
+    그안에 있는 초기값에 접근하려면 .current로 들어가야됨 */}
+      <div
+       style={style}>
+        ref 영역입니다. {countRef.current}
+        <br />
+        <button onClick={plusRefCountButtonHandler}> ref 증가</button>
+      </div>
+    </>
   );
 }
 
