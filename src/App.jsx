6b4c88c4174257d107af7dc58de0dcf21dd71ff1
heexohee <rsoy2918@naver.com>
2023-07-01 14:10:52 +0900

    useState와 useRef를 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
 import "./App.css";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const style = {
-  border: "1px solid black",
-  margin: "10px",
-  padding: "10px",
-}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const countRef = useRef(0);
+const idRef = useRef('');
+const pwRef = useRef('');
 
-const plusStateCountButtonHandler = () => {
-setCount(count + 1 );
-}
-const plusRefCountButtonHandler = () => {
-  countRef.current++;
-  console.log(countRef.current);
-}
 
-  return (
-    <>
-      <div
-        style={style}
-      >
-        state 영역입니다.{count}
-        <br />
-        <button onClick={plusStateCountButtonHandler}> state 증가</button>
-      </div>
-      {/* countRef는 객체, 객체가 만들어진 거고 
-    그안에 있는 초기값에 접근하려면 .current로 들어가야됨 */}
-      <div
-       style={style}>
-        ref 영역입니다. {countRef.current}
-        <br />
-        <button onClick={plusRefCountButtonHandler}> ref 증가</button>
-      </div>
-    </>
-  );
+const [id, setId]=useState('');
+
+  //화면이 랜더링 될 때, 어떤 작업을 하고 싶다.! : useEffect!!!
+
+  useEffect(()=>{
+   idRef.current.focus();
+  }, []);
+
+  useEffect(()=>{
+    if (id.length >= 10){
+      pwRef.current.focus();
+    }
+   }, [id]);
+
+
+return (
+<>
+<div>
+ 아이디 : <input 
+ value={id}
+ onChange={(event)=>{
+  setId(event.target.value)
+ }}
+ type="text" ref={idRef}/>
+</div>
+<div>
+패스워드 : <input type="password" ref={pwRef}/>
+</div>
+</>
+);
 }
 
 export default App;
